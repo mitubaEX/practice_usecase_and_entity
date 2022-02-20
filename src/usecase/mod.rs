0d85bb5709c6c6f8b6d12f25e7d2ab6ff5494b3d
mitubaEX <g1344955@cse.kyoto-su.ac.jp>
2022-02-20 22:42:25 +0900

    split module

--- src/usecase/mod.rs
+++ src/usecase/mod.rs
@@ -1,12 +1,1 @@
-use crate::entity::{Taxi};
-
-pub fn find_taxi(taxi_id: u8) -> Taxi {
-    return Taxi {
-        id: taxi_id
-    }
-}
-
-#[test]
-fn test_find_taxi() {
-    assert_eq!(find_taxi(1).id, 1)
-}
+pub mod find_taxi;
